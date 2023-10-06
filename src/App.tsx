d46f5491919b32a70e48565e9007829b29fc6d05
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-07 00:16:23 +0800

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <>
-      <h1>Hellow Word</h1>
-      <MovieCard/>
-    </>
-  )
+    <Grid
+      templateAreas={`"nav nav" 
+                      "aside recommendation"
+                      "aside main"`}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+
+      <GridItem area="recommendation" bg="blue">
+        Recommendation
+      </GridItem>
+
+      <GridItem area="main" bg="pink.300">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
