649dad2d4a8efdcb6af83b63d61a2587cdf14a33
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-08 02:15:29 +0800

    Clean Up Genres

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,4 +1,4 @@
-import { Button, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, List, ListItem, Text, Heading } from "@chakra-ui/react";
 import genres from "../data/genres";
 
 interface Props {
@@ -8,22 +8,27 @@ interface Props {
 
 const GenresList = ({ selectedGenre, onSelectGenre }: Props) => {
   return (
-    <List>
-      {genres.map((genre) => (
-        <ListItem key={genre} paddingY="5px">
-          <Button
-            fontSize="base"
-            variant="link"
-            textAlign="left"
-            whiteSpace="normal"
-            fontWeight={genre === selectedGenre ? "bold" : "normal"}
-            onClick={() => onSelectGenre(genre === "All" ? "" : genre)}
-          >
-            <Text>{genre}</Text>
-          </Button>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="lg" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => (
+          <ListItem key={genre} paddingY="5px">
+            <Button
+              fontSize="base"
+              variant="link"
+              textAlign="left"
+              whiteSpace="normal"
+              fontWeight={genre === selectedGenre ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre === "All" ? "" : genre)}
+            >
+              <Text>{genre}</Text>
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
