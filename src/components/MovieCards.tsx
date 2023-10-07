966f17843155abfc1c02b6e2a8dcc024f09e4ef8
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-07 16:43:13 +0800

    add release year

--- src/components/MovieCards.tsx
+++ src/components/MovieCards.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Movie } from "../hooks/useMovie";
-
+import ReleaseYear from "./releaseYear";
 interface Props {
   movie: Movie;
 }
@@ -10,7 +10,8 @@ const MovieCard = ({ movie }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={movie.image_url} />
       <CardBody>
-        <Heading fontSize="xl">{movie.title}</Heading>
+        <ReleaseYear release_year={movie.release_year} />
+        <Heading fontSize="lg">{movie.title}</Heading>
       </CardBody>
     </Card>
   );
