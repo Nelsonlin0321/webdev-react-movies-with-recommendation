35045a33a25acb31906d30db5e928ace6196d78d
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-08 14:23:53 +0800

    update Color Mode Switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,9 @@ const ColorModeSwitch = () => {
         isChecked={colorMode == "dark"}
         onChange={() => toggleColorMode()}
       ></Switch>
-      <Text>{colorMode === "dark" ? "Dark Mode" : "Light Mode"}</Text>
+      <Text margin={0}>
+        {colorMode === "dark" ? "Dark Mode" : "Light Mode"}
+      </Text>
     </HStack>
   );
 };
