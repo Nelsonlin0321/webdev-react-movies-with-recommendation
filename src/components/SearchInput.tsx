554082c8e623ae11a954acbb8c3f7aad5bfaff8d
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-11-04 02:52:17 +0800

    replace placeholder

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -21,7 +21,7 @@ const SearchInput = ({ onSearch }: Props) => {
         <Input
           ref={ref}
           borderRadius={20}
-          placeholder="Search Games..."
+          placeholder="Search Movies..."
           variant="filled"
         ></Input>
       </InputGroup>
